104f8ef319e41b2e8e36c01fc757bd0b22c78855
prolic <saschaprolic@googlemail.com>
2016-12-02 19:13:11 +0800

    add missing license header

--- tests/Container/PDOSnapshotStoreFactoryTest.php
+++ tests/Container/PDOSnapshotStoreFactoryTest.php
@@ -1,6 +1,15 @@
 <?php
+/**
+ * This file is part of the prooph/pdo-snapshot-store.
+ * (c) 2016-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2016-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 declare(strict_types=1);
+
 namespace ProophTest\PDO\SnapshotStore\Container;
 
 use Interop\Container\ContainerInterface;
